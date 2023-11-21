3aef4e67ff990a0aef3dc26183120d7733e967ee
ali-hbl <nwah.ally@gmail.com>
2023-11-21 15:01:05 +0100

    clean code

--- utils/index.ts
+++ utils/index.ts
@@ -3,17 +3,21 @@ import { CarProps, FilterProps } from '@/types';
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  const headers = {
-    'X-RapidAPI-Key': '44e00593fcmsh4760f0e5fd96975p1156b6jsncea5cded23d8',
+  // Set the required headers for the API request
+  const headers: HeadersInit = {
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
+  // Set the required headers for the API request
   const response = await fetch(
     `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     },
   );
+
+  // Parse the response as JSON
   const result = await response.json();
 
   return result;
